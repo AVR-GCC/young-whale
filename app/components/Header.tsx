3b893a10f51962e7f5c5fe11d80c3692e7c121ac
AVR-GCC <ogoun.d@gmail.com>
2026-07-26 00:25:42 +0300

    Header for mobile and desktop

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -178,7 +178,7 @@ export default function Header({
   )
 
   return (
-    <header className="pt-2 pb-1.5 w-full border-b border-[#1E293B]/25 bg-[#000000] backdrop-blur-md sticky top-0 z-40">
+    <header className="pt-2 pb-1.5 w-full sm:border-b border-[#1E293B]/25 bg-[#000000] sm:bg-[#070A10]/50 backdrop-blur-md sticky top-0 z-40">
       <div className="max-w-7xl mx-auto w-full px-4 flex items-center justify-between gap-4 relative h-10 md:h-12">
         {title}
         {timer}
