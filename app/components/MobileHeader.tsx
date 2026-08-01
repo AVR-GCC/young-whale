bada86fc02f420867fe4e3f0277aba4912992d60
AVR-GCC <ogoun.d@gmail.com>
2026-08-01 16:30:49 +0300

    All transforms to all

--- app/components/MobileHeader.tsx
+++ app/components/MobileHeader.tsx
@@ -17,13 +17,13 @@ export default function MobileHeader({
   const isInviteModalOpen = settingsView === 'invite' && isSettingsOpen
 
   const title = (
-    <div className={`fixed left-0 transition-transform duration-1000 ease-out ${isMobileOverlayOpen ? '-translate-x-60' : 'translate-x-5'}`}>
+    <div className={`fixed left-0 transition-all duration-1000 ease-out ${isMobileOverlayOpen ? '-translate-x-60' : 'translate-x-5'}`}>
       <HeaderTitle isMobile isMobileOverlayOpen={isMobileOverlayOpen} />
     </div>
   );
 
   const timer = (
-    <div className={`fixed left-0 transition-transform duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-[calc(-10rem_-_50%)]' : 'translate-x-[calc(50vw_-_50%)]'}`}>
+    <div className={`fixed left-0 transition-all duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-[calc(-10rem_-_50%)]' : 'translate-x-[calc(50vw_-_50%)]'}`}>
       <div className="py-1 px-2.5 flex-1 flex flex-col items-center transition-all duration-1000 ease-in-out">
         <span className="font-oxanium text-[11px] font-bold text-[#FFFFFF] uppercase">NEXT WAVE</span>
         <span className="font-oxanium text-xs font-semibold text-[#F8FAFC] tracking-widest leading-none">
@@ -34,7 +34,7 @@ export default function MobileHeader({
   );
 
   const whale = (
-    <div className={`fixed left-0 transition-transform duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-5' : 'translate-x-[calc(100vw_-_4.5rem_-_100%)]'}`}>
+    <div className={`fixed left-0 transition-all duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-5' : 'translate-x-[calc(100vw_-_4.5rem_-_100%)]'}`}>
       <WhaleIcon
         isInviteModalOpen={isInviteModalOpen}
         onClickAction={() => {
@@ -54,7 +54,7 @@ export default function MobileHeader({
   );
 
   const settings = (
-    <div className={`fixed left-0 transition-transform duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-[calc(100vw_-_1rem_-_100%)]' : 'translate-x-[calc(100vw_-_2.5rem_-_100%)]'}`}>
+    <div className={`fixed left-0 transition-all duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-[calc(100vw_-_1rem_-_100%)]' : 'translate-x-[calc(100vw_-_2.5rem_-_100%)]'}`}>
       <SettingsButton
         isInviteModalOpen={isInviteModalOpen}
         isSettingsOpen={isSettingsOpen}
@@ -66,7 +66,7 @@ export default function MobileHeader({
   );
 
   const search = (
-    <div className={`fixed left-0 flex transition-transform duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-[100vw]' : (isSearchOpen ? 'translate-x-[94vw]' : 'translate-x-[calc(100vw_-_0.7rem_-_100%)]')}`}>
+    <div className={`fixed left-0 flex transition-all duration-1000 ease-out ${isMobileOverlayOpen ? 'translate-x-[100vw]' : (isSearchOpen ? 'translate-x-[94vw]' : 'translate-x-[calc(100vw_-_0.7rem_-_100%)]')}`}>
       <SearchButton
         isSearchOpen={isSearchOpen}
         setIsSearchOpenAction={setIsSearchOpenAction}
