5ca6b1dfe1a12d2769ceda655d4949826cfeb036
AVR-GCC <ogoun.d@gmail.com>
2026-08-23 14:13:13 +0300

    metadata json-ld script

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
 import { Metadata } from 'next'
 import HomePageClient from './components/HomePageClient'
 
+const HOMEPAGE_DESCRIPTION =
+  'YoungWhale is your daily Crypto Intelligence Terminal for discovering the newest coins.'
+
 export const metadata: Metadata = {
+  description: HOMEPAGE_DESCRIPTION,
+  openGraph: {
+    description: HOMEPAGE_DESCRIPTION,
+  },
   alternates: {
     canonical: 'https://youngwhale.io/',
   },
 }
 
 export default function Home() {
-  return <HomePageClient />
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Young Whale',
+    url: 'https://youngwhale.io/',
+    description: HOMEPAGE_DESCRIPTION,
+  }
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <HomePageClient />
+    </>
+  )
 }
